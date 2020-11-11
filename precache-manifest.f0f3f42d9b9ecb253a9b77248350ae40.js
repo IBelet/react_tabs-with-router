5c5c09de2bca129abaed37a89752441a19aa6679
Ivan Beletskiy <beletsky02@gmail.com>
2020-11-11 21:29:08 +0200

    Updates

---
+++ precache-manifest.f0f3f42d9b9ecb253a9b77248350ae40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1de7da11357b6db4e1c624489903b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed70f316a914d4b4677c",
+    "url": "./static/css/main.eb343945.chunk.css"
+  },
+  {
+    "revision": "d05f1c791143e64d5ac9",
+    "url": "./static/js/2.2e08967c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.2e08967c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed70f316a914d4b4677c",
+    "url": "./static/js/main.d51b1248.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
